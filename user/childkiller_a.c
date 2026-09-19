2e702da120f76f5aaabe8ac96a9685ae150709fd
Nicholay Shestakov <nicholas.shestakov@gmail.com>
2026-09-19 18:15:08 +0300

    Add error descriptions

--- user/childkiller_a.c
+++ user/childkiller_a.c
@@ -9,6 +9,7 @@ main(int argc, char *argv[])
     exit(-1);
   } else if (child_pid == 0) {
     if (pause(100) == -1) {
+      printf("Child process was killed.\n");
       exit(-1);
     }
     exit(1);
@@ -18,6 +19,7 @@ main(int argc, char *argv[])
   int child_status;
   int wait_res = wait(&child_status);
   if (wait_res == -1) {
+    printf("Child processes was not exists.\n");
     exit(-1);
   }
   printf("Ended process pid: %d\n", wait_res);
--- user/childkiller_b.c
+++ user/childkiller_b.c
@@ -9,6 +9,7 @@ main(int argc, char *argv[])
     exit(-1);
   } else if (child_pid == 0) {
     if (pause(100) == -1) {
+      printf("Child process was killed.\n");
       exit(-1);
     }
     exit(1);
@@ -16,11 +17,13 @@ main(int argc, char *argv[])
   printf("Parent pid: %d\n", getpid());
   printf("Child pid: %d\n", child_pid);
   if (kill(child_pid) == -1) {
+    printf("Process with given pid does not exists.\n");
     exit(-1);
   }
   int child_status;
   int wait_res = wait(&child_status);
   if (wait_res == -1) {
+    printf("Child processes was not exists.\n");
     exit(-1);
   }
   printf("Ended process pid: %d\n", wait_res);
